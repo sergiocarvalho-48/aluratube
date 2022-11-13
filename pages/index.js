d66f5a1a6ef164cabac84d2a12a62eee0ca96e59
Sergio Carvalho <srcarvalho52@gmail.com>
2022-11-13 12:48:15 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import config from "../config.json"
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-
-    const estiloDaHomePage = { 
-        //backgroundColor: "red" 
-    };
-
     const [valorDoFiltro, setValorDoFiltro] = React.useState("")
-
-    //console.log(config.playlists);
-
 
     return (
         <>
-        <CSSReset />
         <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -38,6 +28,7 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
     img {
         width: 80px;
         height: 80px;
